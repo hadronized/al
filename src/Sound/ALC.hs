8aab89dbce116606533a67083d026de79560e920
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-02-28 13:29:10 +0100

    Added export-module ALC.

---
+++ src/Sound/ALC.hs
@@ -0,0 +1,27 @@
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (C) 2015 Dimitri Sabadie
+-- License     : BSD3
+--
+-- Maintainer  : Dimitri Sabadie <dimitri.sabadie@gmail.com>
+-- Stability   : experimental
+-- Portability : portable
+----------------------------------------------------------------------------
+
+module Sound.ALC (
+    module Sound.ALC.Context
+  , module Sound.ALC.Defines
+  , module Sound.ALC.Device
+  , module Sound.ALC.Errors
+  , module Sound.ALC.Extensions
+  , module Sound.ALC.State
+  , module Sound.ALC.Types
+  ) where
+
+import Sound.ALC.Context
+import Sound.ALC.Defines
+import Sound.ALC.Device
+import Sound.ALC.Errors
+import Sound.ALC.Extensions
+import Sound.ALC.State
+import Sound.ALC.Types
