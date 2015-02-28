f9af10789f161943ee9bbbf3a677eff3f9b27dea
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-02-28 13:27:15 +0100

    Added export-module AL.

---
+++ src/Sound/AL.hs
@@ -0,0 +1,31 @@
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : (C) 2015 Dimitri Sabadie
+-- License     : BSD3
+--
+-- Maintainer  : Dimitri Sabadie <dimitri.sabadie@gmail.com>
+-- Stability   : experimental
+-- Portability : portable
+----------------------------------------------------------------------------
+
+module Sound.AL (
+    module Sound.AL.Buffer
+  , module Sound.AL.Defines
+  , module Sound.AL.EAX
+  , module Sound.AL.Errors
+  , module Sound.AL.Extensions
+  , module Sound.AL.Listener
+  , module Sound.AL.Source
+  , module Sound.AL.State
+  , module Sound.AL.Types
+  ) where
+
+import Sound.AL.Buffer
+import Sound.AL.Defines
+import Sound.AL.EAX
+import Sound.AL.Errors
+import Sound.AL.Extensions
+import Sound.AL.Listener
+import Sound.AL.Source
+import Sound.AL.State
+import Sound.AL.Types
